c118765b14a457ffe74efd0b61899f0d8ffb73d3
Kelvin Gomo <kelvingomo@gmail.com>
2022-12-28 04:22:19 +0200

    Added threading and API request to social media.

--- app.py
+++ app.py
@@ -1,9 +1,43 @@
 from flask import Flask
+import requests, logging
+from queue import Queue
+from threading import Thread
 
 app = Flask(__name__)
+
+result = []
+# Queue to hold the urls
+q = Queue()
+urls = [
+    'https://takehome.io/twitter',
+    'https://takehome.io/facebook',
+    'https://takehome.io/instagram'
+]
+num_threads = min(20, len(urls))
+
+
+def get_posts(q, thread_num):
+    """
+    Method to get social media posts.
+    :return:
+    """
+    while True:
+        task = q.get()
+        data = requests.get(task)
+        result.append(data.json())
+        q.task_done()
+        print(f'Thread #{thread_num} is doing task #{task} in the queue.')
+
 
 @app.route("/")
 def social_network_activity():
     # TODO: your code here
-    json_response = {}
+    for i in range(4):
+        worker = Thread(target=get_posts, args=(q, i,), daemon=True)
+        worker.start()
+
+    logging.info('All tasks completed.')
+    q.join()
+    json_response = result  # {}
+
     return json_response
